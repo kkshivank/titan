dc25eb37fdddcd381b2d9cfdec6a32838389eed0
Aman Makhija <amanmakhija2205@gmail.com>
2024-05-16 02:31:52 +0530

    first commit

---
+++ app/(default)/page.tsx
@@ -0,0 +1,20 @@
+export const metadata = {
+  title: 'Home - Open PRO',
+  description: 'Page description',
+}
+
+import Hero from '@/components/hero'
+import Features from '@/components/features'
+import Contact from '@/components/contact'
+import Zigzag from '@/components/zigzag'
+
+export default function Home() {
+  return (
+    <>
+      <Hero />
+      <Features />
+      <Zigzag />
+      <Contact />
+    </>
+  )
+}
---
+++ components/contact.tsx
@@ -0,0 +1,11 @@
+export default function Contact() {
+  return (
+    <section id="contact">
+      <div className="max-w-6xl py-12 mx-auto px-4 sm:px-6">
+
+        <div className="visme_d" data-title="Email Contact Form" data-url="jwod9epy-email-contact-form" data-domain="forms" data-full-page="false" data-min-height="500px" data-form-id="63259"></div><script src="https://static-bundles.visme.co/forms/vismeforms-embed.js"></script>
+
+      </div>
+    </section>
+  )
+}
